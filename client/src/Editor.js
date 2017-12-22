d0002e71edd72a265072a9a66f212e8304d50708
Andrew Liebchen <andrewliebchen@gmail.com>
2017-12-21 21:51:16 -0800

    Add editor

--- client/src/Editor.js
+++ client/src/Editor.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import AceEditor from "react-ace";
+
+import "brace/mode/jsx";
+import "brace/theme/tomorrow_night_eighties";
 
 import "./Editor.css";
 
-const Editor = props => <div className="Editor p1 h100">Editor</div>;
+class Editor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 0,
+      height: 0
+    };
+  }
+
+  _resizeEditor() {
+    const editor = ReactDOM.findDOMNode(this.refs.Editor);
+    this.setState({ width: editor.offsetWidth, height: editor.offsetHeight });
+  }
+
+  componentDidMount() {
+    this._resizeEditor();
+  }
+
+  render() {
+    return (
+      <div className="Editor h100" ref="Editor">
+        <AceEditor
+          width={`${this.state.width}px`}
+          height={`${this.state.height}px`}
+          mode="jsx"
+          theme="tomorrow_night_eighties"
+          fontSize={16}
+          focus
+          tabSize={2}
+          options={{
+            displayIndentGuides: true
+          }}
+          onChange={value => console.log(value)}
+          style={{ position: "absolute" }}
+        />
+      </div>
+    );
+  }
+}
 
 export default Editor;
